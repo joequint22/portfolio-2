7165749b52c99166a8e4d678d7618d44a24e3225
joseph quintiliano <jquintiliano1@gmail.com>
2023-09-21 17:08:34 -0400

    test deploy commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import SocialLinks from './components/SocialLinks'
