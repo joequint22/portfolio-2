800216581307e7eef29db501c70ec95463c5eb03
joseph quintiliano <jquintiliano1@gmail.com>
2023-09-23 02:22:03 -0400

    title edit commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className='max-w-screen-lg  py-36 relative mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
         <div className='flex flex-col justify-center h-full'>
           <h2 className='text-4xl sm:text-7xl font-bold text-white '>
-            I'm a Junior Front End Web Developer
+           Junior Front End Web Developer
           </h2>
 
           <p className='pt-10 text-gray-500 py-4 max-w-md '> 
