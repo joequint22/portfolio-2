135ac1ec63b03813f10a7f89f44d0d0af7a107d6
joseph quintiliano <jquintiliano1@gmail.com>
2023-09-23 21:01:51 -0400

    deploy menu commit

--- src/components/Links/Link.tsx
+++ src/components/Links/Link.tsx
@@ -1,12 +1,14 @@
 
 const Link = (props: {id: number, link: string}) => {
   return (
-    <li
-      key={props.id}
-      className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200"
-    >
-      {props.link}
-    </li>
+    <a href={`#${props.link}`}>
+      <li
+        key={props.id}
+        className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200"
+      >
+        {props.link}
+      </li>
+    </a>
   )
 }
 
