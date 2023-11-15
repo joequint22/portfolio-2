d2f558d7b05d1feeb1ce671758cbe8a67c05f4c6
joseph quintiliano <jquintiliano1@gmail.com>
2023-11-15 08:33:12 -0500

    links update

--- src/components/Links/Link.tsx
+++ src/components/Links/Link.tsx
@@ -2,7 +2,7 @@ import { SetStateAction } from "react";
 
 const Link = (props: { id: number; link: string
   download?: boolean;
-  href?: string;
+  href?: string | undefined;
   target?: string;
   rel?: string;
   demo?: string;
@@ -16,13 +16,13 @@ const Link = (props: { id: number; link: string
 } 
   return (
     <a 
-      href={props.id !== 6 ? `#${props.link}`: `${props.href}`}
+      href={props.id !== 6 ? `#${props.link}`: undefined}
       download={props.download}
       target={props.target}
       rel={props.rel}
-      onClick={props.id !== 6 ? () => null : () => handleButtonClick('./joeq-resume.pdf')}
-    >
+      >
       <li
+      onClick={props.id !== 6 ? () => null : () => handleButtonClick('./joeq-resume.pdf')}
         key={props.id}
         className="px-4 gap-3 cursor-pointer capitalize font-medium text-white hover:text-green-200 hover:scale-125 duration-500"
         
